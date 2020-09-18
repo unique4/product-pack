fa440a924fdeff356a753d830152a16e97c70e21
Que Nguyen <unique@nutishop.com>
2020-09-18 16:54:15 -0700

    commit all

--- stock_product_pack/models/product_product.py
+++ stock_product_pack/models/product_product.py
@@ -17,6 +17,7 @@ class ProductProduct(models.Model):
         for product in packs.with_context(prefetch_fields=False):
             pack_qty_available = []
             pack_virtual_available = []
+            pack_free_qty = []
             subproducts = product.pack_line_ids.filtered(
                 lambda p: p.product_id.type == 'product')
             for subproduct in subproducts:
@@ -27,6 +28,8 @@ class ProductProduct(models.Model):
                         subproduct_stock.qty_available / sub_qty))
                     pack_virtual_available.append(math.floor(
                         subproduct_stock.virtual_available / sub_qty))
+                    pack_free_qty.append(math.floor(
+                        subproduct_stock.free_qty / sub_qty))
             res[product.id] = {
                 'qty_available': (
                     pack_qty_available and min(pack_qty_available) or False),
@@ -35,5 +38,7 @@ class ProductProduct(models.Model):
                 'virtual_available': (
                     pack_virtual_available and
                     min(pack_virtual_available) or False),
+                'free_qty': (
+                    pack_free_qty and min(pack_free_qty) or False),
             }
         return res
